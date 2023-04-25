5393635264a743743fdb3a8bb0086b77c53cb36f
Zion Danielson <zionsdocs@gmail.com>
2023-04-25 10:44:34 -0400

    net_worth.js

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,17 @@
+"use sttrict";
+
+//Calculating Networth
+
+//Liabilities
+var bills = $700
+var expenses = $200
+
+//Assets
+var assets = $10000
+var liabilities = ("bills" + "expenses")
+
+//Networth
+var Networth = ("assets" - "liabilities")
+
+
+console.log(Networth)
