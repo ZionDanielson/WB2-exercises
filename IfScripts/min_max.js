c857abeea6c53bf5133ba5f7f730733b342cc459
Zion Danielson <zionsdocs@gmail.com>
2023-04-27 13:50:08 -0400

    min_max.js

---
+++ IfScripts/min_max.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// 1. Get known variables
+
+let a = 25;
+let b = 13;
+let c = 19;
+
+// 2. Calculate the unknown
+
+// Find the smallest number
+let smallest;
+if (a < b) {
+  if (a < c) {
+    smallest = a;
+  } 
+  else {
+    smallest = c;
+  }
+} 
+else {
+  if (b < c) {
+    smallest = b;
+  } 
+  else {
+    smallest = c;
+  }
+}
+console.log("The smallest number is: " + smallest);
+
+// Find the largest number
+let largest;
+if (a > b) {
+  if (a > c) {
+    largest = a;
+  } 
+  else {
+    largest = c;
+  }
+} 
+else {
+  if (b > c) {
+    largest = b;
+  } 
+    else {
+    largest = c;
+  }
+}
+console.log("The largest number is: " + largest);
